b5c5df161dbad9cdebf82ee1b69e4e429a95fd77
Manuel Estevez <67356911+mestevez19@users.noreply.github.com>
2020-08-09 18:02:30 -0500

    Plot2 png/r File

---
+++ plot2.R
@@ -0,0 +1,24 @@
+library("data.table")
+
+
+#Reads in data from file (folder named datos)
+datos <- data.table::fread(input = "datos/household_power_consumption.txt"
+                             , na.strings="?")
+
+# Prevents Scientific Notation
+datos[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+#enable POSIXct being filtered by time of day
+datos[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+datos2 <- datos[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+
+plot(x = datos2[, dateTime]
+     , y = datos2[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
